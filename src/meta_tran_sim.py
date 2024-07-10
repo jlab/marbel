8647ba3c87f96e62857281d04a9e4141f65f73bc
Timo Wentong Lin <timo.lin@cb.jlug.de>
2024-07-10 14:04:10 +0200

    add build files

---
+++ src/meta_tran_sim.py
@@ -0,0 +1,6 @@
+#Open Questions:
+
+#how to incorporate the ground data?
+#I should add flags for easy evaluation
+#Add the statistical simulations, maybe use R code for that?
+#Can the R libraries be also packaged
